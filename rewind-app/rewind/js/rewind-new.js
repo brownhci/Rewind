b506423b7de6d9b50516460598d0b31323faa48d
Kelly Wang <kelly_wang@brown.edu>
2017-10-25 15:30:12 -0400

    added instructions modal

--- rewind-app/rewind/js/rewind-new.js
+++ rewind-app/rewind/js/rewind-new.js
@@ -257,6 +257,78 @@ function initMap(latVal, lngVal, response) {
 
     document.getElementById("sample").addEventListener("click", handleDefaultFile);
 
+    
+    /** MODAL STUFF**/
+
+    // Get the modal
+    var modal = document.getElementById('instructions-modal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    document.getElementById("instructions").addEventListener("click", showInstructions);
+
+    function showInstructions(){
+        console.log('instructions clicked')
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
+    var prevPage = document.getElementsByClassName("prev")[0];
+    var nextPage = document.getElementsByClassName("next")[0];
+    prevPage.addEventListener("click", clickPrevPage);
+    nextPage.addEventListener("click", clickNextPage);
+
+    function clickNextPage(){
+        plusSlides(1);
+    }
+
+    function clickPrevPage(){
+        plusSlides(-1);
+    }
+
+
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1} 
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none"; 
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block"; 
+      dots[slideIndex-1].className += " active";
+    }
+
+    /*****/
+
     function handleDefaultFile(){
         $.getJSON( "short.json", function( data ) {
                 days = parseLocationJson(data);
